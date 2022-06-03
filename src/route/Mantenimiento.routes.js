cf2c37b900ed7164ff80030a8ec37aa35d4e321e
William Ricardo Enciso Bautista <william.enciso@servimeterssa.com>
2022-06-03 18:55:51 -0500

    ADD CRUD Mantenimiento

--- src/route/Mantenimiento.routes.js
+++ src/route/Mantenimiento.routes.js
@@ -3,11 +3,16 @@ const { Router } = require('express');
 module.exports = function ({ MantenimientoController }) {
     const router = Router();
 
-    router.post('/insert', MantenimientoController.createMantenimiento);
+    router.post('/insert', MantenimientoController.createMantenimiento); 
     router.patch('/update', MantenimientoController.updateMantenimiento);
     router.delete('/delete/:id', MantenimientoController.deleteMantenimiento);
-    router.get('/:id', MantenimientoController.getMantenimiento);
+    router.get('/:id', MantenimientoController.getMantenimientoByID);
+    router.get('/equipo/:id_equipo', MantenimientoController.getMantenimientoByEquipo);
     router.get('/', MantenimientoController.getAllMantenimientos);
+    router.get('/all/preventivos', MantenimientoController.getMantenimientosPreventivos);
+    router.get('/all/correctivos', MantenimientoController.getMantenimientosCorrectivos);
+    router.get('/all/tecnico/:id_tecnico', MantenimientoController.getMantenimientoByTecnico);
+    router.get('/all/fecha/:fecha_in/:fecha_fin', MantenimientoController.getMantenimientoByFechas);
 
     return router; 
 };
--- src/service/BD.service.js
+++ src/service/BD.service.js
@@ -34,7 +34,7 @@ class BDService {
             return error;
         }
         
-    } 
+    }
     
 }
 
